da335158673a0c7ce18f172b283ff07e1893c611
Md. Sajid Ahmed <sajidr.u110011@gmail.com>
2025-12-23 13:07:44 +0600

    initial navbar

---
+++ hero_app/src/Routes/Routes.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { createBrowserRouter } from "react-router";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import Root from "../Pages/Root/Root";
+import Home from "../Pages/Home/Home";
+import Apps from "../Pages/Apps/Apps";
+import Installation from "../Pages/Installation/Installation";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        index: true,
+        path: "/",
+        Component: Home,
+      },
+      {
+        path: "/apps",
+        Component: Apps,
+      },
+      {
+        path: "/installation",
+        Component: Installation,
+      },
+    ],
+  },
+]);
